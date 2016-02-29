a62dc0a36afa094225178fb0c2ca3f3f9801cfef
vtt01uk <vtt01uk@gmail.com>
2016-02-29 16:51:39 +0000

    Added more bootstrap to index and show pages

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,4 @@
 class Listing < ActiveRecord::Base
-  has_attached_file :image, styles: { medium: "200x200", thumb: "50x50>" }, default_url: "image_na.jpg"
+  has_attached_file :image, styles: { medium: "200x200", thumb: "50x50>" }, default_url: "/image_na.jpg"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
