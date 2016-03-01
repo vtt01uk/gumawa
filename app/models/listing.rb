4759237256c374ac1f58d9f659ab0543f1c9f995
vtt01uk <vtt01uk@gmail.com>
2016-03-01 18:26:01 +0000

    Added more validations to Listing and User tables

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,12 @@
 class Listing < ActiveRecord::Base
   has_attached_file :image, styles: { medium: "200x200", thumb: "50x50>" }, default_url: "/image_na.jpg"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  
+  validates :name, :description, :price, presence: true
+  
+  #check to see if price is actually a number and positive
+  validates :price, numericality: { greater_than: 0 }
+  
+  #validates images 
+  validates_attachment_presence :image
 end
